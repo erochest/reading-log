d2192cbd3b933a5f2cc1a326f4a385f85dfc0b01
Eric Rochester <erochest@virginia.edu>
2015-09-22 11:32:04 -0400

    Added generator for turtle and JSON-LD output.

--- _plugins/RdfaGenerator.rb
+++ _plugins/RdfaGenerator.rb
@@ -7,7 +7,36 @@ module Jekyll
     safe true
 
     def generate(site)
+      require 'fileutils'
+      require 'json'
+      require 'json/ld'
       require 'rdf'
+      require 'rdf/turtle'
+      require 'rdf/rdfa'
+
+      converter = site.getConverterImpl(Jekyll::Converters::Markdown)
+
+      graph = RDF::Graph.new
+      site.posts.each do |post|
+        html = converter.convert(post.content)
+        graph.from_rdfa html
+      end
+
+      dirname = "_linked-data"
+      FileUtils.mkpath(dirname) unless File.exists?(dirname)
+
+      File.open("#{dirname}/posts.ttl", 'w') do |file|
+        file.write(graph.to_ttl)
+      end
+      site.static_files << Jekyll::StaticFile.new(
+        site, "_linked-data", "", "posts.ttl"
+      )
+      File.open("#{dirname}/posts.json", 'w') do |file|
+        file.write(graph.dump(:jsonld, standard_prefixes: true))
+      end
+      site.static_files << Jekyll::StaticFile.new(
+        site, "_linked-data", "", "posts.json"
+      )
     end
   end
 end
