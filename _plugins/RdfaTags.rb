b2bdd3d1973ab2fe2a70ad87a9af3a59b69dd3c9
Eric Rochester <erochest@virginia.edu>
2015-10-01 16:42:32 -0400

    Added {% vocab %} block.

--- _plugins/RdfaTags.rb
+++ _plugins/RdfaTags.rb
@@ -1,8 +1,20 @@
 module Jekyll
-  class RdfaTypeOfTag < Liquid::Tag
+  # https://github.com/Shopify/liquid/wiki/Liquid-for-Programmers
+  # https://github.com/jekyll/jekyll/issues/1380
+  class RdfaVocabBlock < Liquid::Block
+    def initialize(tag_name, markup, tokens)
+      @url = markup
+      super
+    end
+
     def render(context)
+      body = super
+      site = context.registers[:site]
+      converter = site.getConverterImpl(Jekyll::Converters::Markdown)
+      body = converter.convert(super(context))
+      "<div vocab=\"#{@url}\">\n\n#{body}\n\n</div>"
     end
   end
 end
 
-Liquid::Template.register_tag('typeof', Jekyll::RdfaTypeOfTag)
+Liquid::Template.register_tag('vocab', Jekyll::RdfaVocabBlock)
