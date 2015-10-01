cd5fce9059f17a39c0317071e646ddf1649979a3
Eric Rochester <erochest@virginia.edu>
2015-10-01 16:54:25 -0400

    First pass at {% rsc_span %} blocks.
    This is for adding a <span> tag with a @resource attribute and
    optionally a @typeof attribute.
    
    Currently, this doesn't run its content through a Markdown converter,
    because they wrap the content in a <p>, which isn't helpful.

--- _plugins/RdfaTags.rb
+++ _plugins/RdfaTags.rb
@@ -1,6 +1,4 @@
 module Jekyll
-  # https://github.com/Shopify/liquid/wiki/Liquid-for-Programmers
-  # https://github.com/jekyll/jekyll/issues/1380
   class RdfaVocabBlock < Liquid::Block
     def initialize(tag_name, markup, tokens)
       @url = markup
@@ -8,13 +6,34 @@ module Jekyll
     end
 
     def render(context)
-      body = super
       site = context.registers[:site]
       converter = site.getConverterImpl(Jekyll::Converters::Markdown)
       body = converter.convert(super(context))
       "<div vocab=\"#{@url}\">\n\n#{body}\n\n</div>"
     end
   end
+
+  class RdfaResourceSpan < Liquid::Block
+    def initialize(tag_name, markup, tokens)
+      params = markup.split ' '
+      @url = params.shift
+      @typeof = params.shift || nil
+      super
+    end
+
+    def render(context)
+      site = context.registers[:site]
+      converter = site.getConverterImpl(Jekyll::Converters::Markdown)
+      body = super
+
+      span = "<span resource=\"#{@url}\""
+      span << " typeof=\"#{@typeof}\"" unless @typeof.nil?
+      span << ">#{body}</span>"
+
+      span
+    end
+  end
 end
 
 Liquid::Template.register_tag('vocab', Jekyll::RdfaVocabBlock)
+Liquid::Template.register_tag('rsc_span', Jekyll::RdfaResourceSpan)
